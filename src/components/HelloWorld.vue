7b8eb360f27e4e16a9fb4a936d283a6fcd1dbb31
Johndave Lausa <johndavelausa@gmail.com>
2024-09-18 19:03:58 -0700

    vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center" justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card class="todo-card" outlined>
+          <v-card-title>
+            <v-text-field
+              v-model="newTodo"
+              label="Add a new task"
+              @keyup.enter="addTodo"
+              solo
+            ></v-text-field>
+          </v-card-title>
+
+          <v-list>
+            <v-list-item-group v-for="(todo, index) in todos" :key="index">
+              <v-list-item>
+                <v-row align="center" no-gutters>
+                  <v-col cols="8" class="text-left px-2 py-4">
+                    <v-list-item-title>{{ todo.text }}</v-list-item-title>
+                  </v-col>
+                  <v-col cols="4" class="text-right">
+                    <v-btn @click="editTodo(index)" icon>
+                      <v-icon>mdi-pencil</v-icon>
+                    </v-btn>
+                    <v-btn @click="deleteTodo(index)" icon>
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim()) {
+        this.todos.push({ text: this.newTodo });
+        this.newTodo = '';
+      }
+    },
+    editTodo(index) {
+      const updatedTodo = prompt('Edit your task:', this.todos[index].text);
+      if (updatedTodo !== null) {
+        this.todos[index].text = updatedTodo;
+      }
+    },
+    deleteTodo(index) {
+      if (confirm('Are you sure you want to delete this task?')) {
+        this.todos.splice(index, 1);
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.todo-card {
+  width: 400px;
+  margin: 20px auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.v-list-item {
+  margin-bottom: 8px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.v-btn {
+  margin-left: 5px;
 }
 </style>
